936d094d448e4288246fb1e7db8e9083370bcbee
Mark Kraus <github@markekraus.com>
2018-11-06 05:01:29 -0800

    Add support for ps_modules to be auto appended to PSModulePath (#4)

--- extensions/PSInvokeBuildTask/task.ps1
+++ extensions/PSInvokeBuildTask/task.ps1
@@ -1,9 +1,19 @@
 [CmdletBinding()]
 Param()
 
+# Append PSModulePath with the Task's ps_modules folder
+$PS_ModulesPath = Join-Path $PSScriptRoot 'ps_modules'
+$env:PSModulePath = '{0}{1}{2}' -f @(
+    $env:PSModulePath,
+    [System.IO.Path]::PathSeparator,
+    $PS_ModulesPath
+)
+
+# Import task.json data
 $TaskJsonFile = Join-Path $PSScriptRoot 'task.json'
 $TaskJsonData = Get-Content -raw $TaskJsonFile | ConvertFrom-Json
 
+# Retrieve task inputs
 $Inputs = @{}
 foreach ($InputItem in $TaskJsonData.Inputs) {
     $Params = @{
